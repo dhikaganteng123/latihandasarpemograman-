daaf1c54e734034889c9effd121cdcb85ee9f75e
dhika dwi nugraha <114630154+dhikaganteng123@users.noreply.github.com>
2022-09-29 11:04:34 +0700

    Add files via upload

---
+++ aplikasibiodata.py
@@ -0,0 +1,11 @@
+#aplikasi biodata create by dhika
+
+#memasukkan nama
+nama = input("Masukan Nama Anda! :")
+kelas = input ("Masukan kelas saat ini : ")
+prodi = input ("prodi saat ini : ")
+TTL = input ("Tempat dan tanggal lahir ; ")
+print("Nama anda adalah : ", nama)
+print("kelas saat ini : ", kelas)
+print("prodi anda : ", prodi)
+print("tempat tanggal lahir : ", TTL)
