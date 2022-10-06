a0da6e766f818e11c55cc94111d6306bf6e19371
dhika dwi nugraha <114630154+dhikaganteng123@users.noreply.github.com>
2022-10-06 09:18:06 +0700

    Add files via upload

---
+++ tugassesi3.py
@@ -0,0 +1,8 @@
+# Tugas sesi 3 by DHIKA
+# Biodata sederhana Dhika Dwi Nugraha
+nama = "Dhika Dwi Nugraha"
+kelas = "TI22A"
+prodi = "Teknik Informatika"
+quote = "Tetap menyerah jangan semangat"
+
+print (nama, kelas, prodi, quote)
